924fa1b098aec417e398e9ab82228b49a9b41424
Nobeen <n.islam.1@outlook.com>
2022-02-25 11:24:02 +0000

    Add tests

---
+++ src/find-shortest-string.spec.ts
@@ -0,0 +1,14 @@
+const findShortestString = require("./find-shortest-string");
+
+test("findShortestString finds the shortest string in an array", () => {
+  expect(findShortestString(["it", "isss", "app", "nice", "day"])).toBe("it");
+  expect(findShortestString(["whyyyy", "hello", "tooooo", "youuuuu"])).toBe(
+    "hello"
+  );
+});
+
+test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
+  expect(findShortestString(["brave", "dance"])).toBe("brave");
+  expect(findShortestString(["holeeeee", "vroles", "troles"])).toBe("vroles");
+  expect(findShortestString(["ok", "sok", "wok", "ek"])).toBe("ok");
+});
